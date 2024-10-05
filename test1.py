63952ca5dc713bffc527a046a2e0cb39b989959d
Drilordubu <diego.gonzalezsilva@gmail.com>
2024-10-05 17:47:41 -0600

    add file mgmt python

--- test1.py
+++ test1.py
@@ -1,4 +1,13 @@
-path='devops/test.txt'
-mydoc=open('path','r')
-fisrtline=mydoc.readlines()
-print(fisrtline)
+path = "/home/ubuntu/devops/test.txt"
+
+mydoc = open(path,'+a')
+print("Que quieres escribir en el archivo?")
+entrar=input()
+mydoc.write(entrar)
+print("quieres ver el archivo? y/n")
+imprimir=input()
+if imprimir is"y":
+    read = mydoc.read()
+    print(read)
+else:
+    print("chido barrio")
